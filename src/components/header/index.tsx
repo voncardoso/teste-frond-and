8d58bc1c611fbb7dc88007c0bbd3781079cd8ac2
Von Harrison <vonharrison26@gmail.com>
2022-02-03 09:30:48 -0300

    menu mobile

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,7 @@
 import {Container, ContainerMoile} from './style';
-
+import {initMenuMobile} from "./script";
 
 export function Header(){
-
-    const menuButton = document.querySelector('.ButtonMobile');
-    const ListMobile  = document.querySelector('.ListMobile ');
-    
-    function menuMobile(){
-        console.log('clicou');
-        menuButton?.classList.toggle('active');
-        ListMobile?.classList.toggle('active');
-    }
-
-   
         
     return(
         <>
@@ -29,7 +18,7 @@ export function Header(){
         </Container>
         <ContainerMoile>
             <div className="divSecundaria"></div>   
-            <button className="ButtonMobile" onClick={menuMobile}></button>
+            <button className="ButtonMobile" onClick={initMenuMobile}></button>
                 <ul className="ListMobile">
                     <li>HEALTHY RECIPES</li>
                     <li>BLOG</li>
---
+++ src/components/header/script.js
@@ -0,0 +1,36 @@
+export function initMenuMobile() {
+    const menuButton = document.querySelector('.ButtonMobile');
+    const List = document.querySelector('.List');
+    const ListMobile  = document.querySelector('.ListMobile ');
+    const ListNav = document.querySelector('.nav');
+    const divSecundaria = document.querySelector('.divSecundaria');
+    const linksInternos = document.querySelectorAll('.ListMobile a[href^="#"]');
+
+       menuButton.classList.toggle('active');
+        ListMobile.classList.toggle('active');
+       divSecundaria.classList.toggle('active'); 
+       
+
+      function fecharMenu(){
+            menuButton.classList.remove('active');
+            List.classList.remove('active');
+            divSecundaria.classList.remove('active'); 
+            ListMobile.classList.toggle('active');
+        }
+        function initScrollSuaveMobile(event){
+          console.log(linksInternos)
+          event.preventDefault();
+          const href = event.currentTarget.getAttribute('href');
+          const section = document.querySelector(href);
+          section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+          });
+        }
+
+        ListNav.addEventListener('click', fecharMenu);
+        divSecundaria.addEventListener('click', fecharMenu);
+        linksInternos.forEach((link) => {
+          link.addEventListener('click', initScrollSuaveMobile);
+        });
+}
